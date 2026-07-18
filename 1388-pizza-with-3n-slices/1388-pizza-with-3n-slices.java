ab9bd1b6df91d5fe7cd733c63c74a005b67b5553
KUMBHAR SHREE SURESH <shreekumbhar023@gmail.com>
2026-07-18 14:01:33 +0530

    Time: 5 ms (100%), Space: 43.5 MB (99.35%) - LeetHub

--- 1388-pizza-with-3n-slices/1388-pizza-with-3n-slices.java
+++ 1388-pizza-with-3n-slices/1388-pizza-with-3n-slices.java
@@ -15,34 +15,77 @@ class Solution {
 
     // }
 
-    public static int solveTab(int[] slices){
+    // public static int solveTab(int[] slices){
+
+    //     int k=slices.length;
+    //     int[][] dp1=new int[k+2][k+2];
+    //     int[][] dp2=new int[k+2][k+2];
+
+    //     //1 case
+    //     for(int index=k-2;index>=0;index--){
+    //         for(int n=1;n<=k/3;n++){
+    //             int take = slices[index] + dp1[index+2][n-1];
+    //             int notTake= 0 + dp1[index+1][n];
+    //             dp1[index][n] = Math.max(take,notTake);
+    //         }
+    //     }
+    //     int case1=dp1[0][k/3];
+
+    //     //2 case
+    //     for(int index=k-1;index>=1;index--){
+    //         for(int n=1;n<=k/3;n++){
+    //             int take = slices[index] + dp2[index+2][n-1];
+    //             int notTake= 0 + dp2[index+1][n];
+    //             dp2[index][n] = Math.max(take,notTake);
+    //         }
+    //     }
+    //     int case2=dp2[1][k/3];
+
+    //     return Math.max(case1,case2);
+    // }
+
+    public static int solveSO(int[] slices){
 
         int k=slices.length;
-        int[][] dp1=new int[k+2][k+2];
-        int[][] dp2=new int[k+2][k+2];
+        int[] prev1=new int[k+2];
+        int[] curr1=new int[k+2];
+        int[] next1=new int[k+2];
 
-        //1 case
+        //case1
         for(int index=k-2;index>=0;index--){
             for(int n=1;n<=k/3;n++){
-                int take = slices[index] + dp1[index+2][n-1];
-                int notTake= 0 + dp1[index+1][n];
-                dp1[index][n] = Math.max(take,notTake);
+                int take = slices[index] + next1[n-1];
+                int notTake=curr1[n];
+                prev1[n]=Math.max(take,notTake);
             }
+            int[] temp=next1;
+            next1=curr1;
+            curr1=prev1;
+            prev1=temp;
         }
-        int case1=dp1[0][k/3];
+        int case1=curr1[k/3];
 
-        //2 case
+        //case2
+        int[] prev2=new int[k+2];
+        int[] curr2=new int[k+2];
+        int[] next2=new int[k+2];
+
         for(int index=k-1;index>=1;index--){
             for(int n=1;n<=k/3;n++){
-                int take = slices[index] + dp2[index+2][n-1];
-                int notTake= 0 + dp2[index+1][n];
-                dp2[index][n] = Math.max(take,notTake);
+                int take = slices[index] + next2[n-1];
+                int notTake=curr2[n];
+                prev2[n]=Math.max(take,notTake);
             }
+            int[] temp=next2;
+            next2=curr2;
+            curr2=prev2;
+            prev2=temp;
         }
-        int case2=dp2[1][k/3];
+        int case2=curr2[k/3];
 
         return Math.max(case1,case2);
     }
+
 
     public int maxSizeSlices(int[] slices) {
 
@@ -50,6 +93,6 @@ class Solution {
         // int case1=solve(slices,0,k-2,k/3);
         // int case2=solve(slices,1,k-1,k/3);
         // return Math.max(case1,case2);
-        return solveTab(slices);
+        return solveSO(slices);
     }
 }
