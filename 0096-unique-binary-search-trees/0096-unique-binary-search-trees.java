d2d08ce89407d7cef5fb111d6f3f27efd2e550e3
KUMBHAR SHREE SURESH <shreekumbhar023@gmail.com>
2026-07-30 12:00:06 +0530

    Time: 0 ms (100%), Space: 41.8 MB (93.19%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -1,21 +1,48 @@
 class Solution {
 
-    public static int solveMem(int n,int[] dp){
+    // public static int solveMem(int n,int[] dp){
 
-        if(n<=1){
-            return 1;
+    //     if(n<=1){
+    //         return 1;
+    //     }
+
+    //     if(dp[n]!=-1){
+    //         return dp[n];
+    //     }
+
+    //     int ans=0;
+    //     for(int i=1;i<=n;i++){
+    //         ans+=solveMem(i-1,dp)*solveMem(n-i,dp);
+    //     }
+
+    //     return dp[n]=ans;
+    // }
+
+    // public static int solveTab(int n){
+
+    //     int[] dp=new int[n+1];
+    //     Arrays.fill(dp,0);
+
+    //     dp[0]=1;
+    //     dp[1]=1;
+
+    //     for(int node=2;node<=n;node++){
+    //         for(int root=1;root<=node;root++){
+    //             dp[node]+=dp[root-1]*dp[node-root];
+    //         }
+    //     }
+    //     return dp[n];
+    // }   
+
+    public static int optimize(int n){
+
+        long ans=1;
+
+        for(int i=0;i<n;i++){
+            ans=ans*(2*n-i)/(i+1);
         }
 
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-
-        int ans=0;
-        for(int i=1;i<=n;i++){
-            ans+=solveMem(i-1,dp)*solveMem(n-i,dp);
-        }
-
-        return dp[n]=ans;
+        return (int) (ans/(n+1));
     }
 
     public int numTrees(int n) {
@@ -31,8 +58,8 @@ class Solution {
         // }
 
         // return ans;
-        int[] dp=new int[n+1];
-        Arrays.fill(dp,-1);
-        return solveMem(n,dp);
+        // int[] dp=new int[n+1];
+        // Arrays.fill(dp,-1);
+        return optimize(n);
     }
 }
