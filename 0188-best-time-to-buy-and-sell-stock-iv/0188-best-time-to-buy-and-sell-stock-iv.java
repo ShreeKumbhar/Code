97cf4a9e138de26bc8ed75465e5b6b5f6be2cefa
KUMBHAR SHREE SURESH <shreekumbhar023@gmail.com>
2026-08-03 14:17:00 +0530

    Time: 2 ms (71.91%), Space: 45.1 MB (57.4%) - LeetHub

---
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -0,0 +1,90 @@
+class Solution {
+
+    // public static int solveRec(int[] arr,int index,int operation,int k){
+
+    //     if(index==arr.length || operation==2*k){
+    //         return 0;
+    //     }
+
+    //     int profit=0;
+    //     if(operation%2==0){
+    //         int buy=-arr[index]+solveRec(arr,index+1,operation+1,k);
+    //         int skip=0+solveRec(arr,index+1,operation,k);
+    //         profit=Math.max(buy,skip);
+    //     }
+    //     else{
+    //         int sell=arr[index]+solveRec(arr,index+1,operation+1,k);
+    //         int skip=0+solveRec(arr,index+1,operation,k);
+    //         profit=Math.max(sell,skip);
+    //     }
+
+    //     return profit;
+    // }
+
+    // Memorization
+    public static int solveMem(int[] arr,int index,int operation,int k,int[][] dp){
+
+        if(index==arr.length || operation==2*k){
+            return 0;
+        }
+
+        if(dp[index][operation]!=-1){
+            return dp[index][operation];
+        }
+
+        int profit=0;
+        if(operation%2==0){
+            int buy=-arr[index]+solveMem(arr,index+1,operation+1,k,dp);
+            int skip=0+solveMem(arr,index+1,operation,k,dp);
+            profit=Math.max(buy,skip);
+        }
+        else{
+            int sell=arr[index]+solveMem(arr,index+1,operation+1,k,dp);
+            int skip=0+solveMem(arr,index+1,operation,k,dp);
+            profit=Math.max(sell,skip);
+        }
+
+        return dp[index][operation]=profit;
+    }
+
+    // tabulation
+    public static int solveTab(int[] arr,int k){
+
+        int n=arr.length;
+        int[][] dp=new int[n+1][k*2+1];
+        for(int[] i:dp){
+            Arrays.fill(i,0);
+        }
+
+        for(int index=n-1;index>=0;index--){
+            for(int operation=2*k-1;operation>=0;operation--){
+                int profit=0;
+                if(operation%2==0){
+                    int buy=-arr[index]+dp[index+1][operation+1];
+                    int skip=0+dp[index+1][operation];
+                    profit=Math.max(buy,skip);
+                }
+                else{
+                    int sell=arr[index]+dp[index+1][operation+1];
+                    int skip=0+dp[index+1][operation];
+                    profit=Math.max(sell,skip);
+                }
+                dp[index][operation]=profit;
+            }
+        }
+
+        return dp[0][0];
+    }
+
+    public int maxProfit(int k, int[] prices) {
+        // return solveRec(prices,0,0,k);
+        int n=prices.length;
+        // int[][] dp=new int[n][2*k];
+        // for(int[] i:dp){
+        //     Arrays.fill(i,-1);
+        // }
+        // return solveMem(prices,0,0,k,dp);
+
+        return solveTab(prices,k);
+    }
+}
